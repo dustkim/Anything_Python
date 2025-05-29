6c332f8e59f1f1786a7fca7dc270c38da1a79a22
dustkim <dnjswlssla78@naver.com>
2025-05-29 19:36:34 +0900

    2025/05/29

--- portfolio/src/Component/Portfolio.js
+++ portfolio/src/Component/Portfolio.js
@@ -1,8 +1,49 @@
 import React from "react";
 import "../CSS/Portfolio.css";
+import portfolio from "../Image/portfolio.png";
+import project1 from "../Image/project1.png";
+import project2 from "../Image/project2.png";
+import project3 from "../Image/project3.png";
 
-const Portfolio = ({ portfolio }) => {
-  return <div className="TotalPortfolioBox"></div>;
+const Portfolio = () => {
+  return (
+    <div className="TotalPortfolioBox">
+      <div className="PortfolioContainer">
+        <img src={portfolio} alt="portfolio" width={100} />
+        <div className="PortfolioContentBox">
+          <div className="Portfolio">
+            <div className="project">
+              <img src={project1} alt="project1" width={300} />
+              <div
+                className="overlay"
+                onClick={() => window.open("https://github.com/dustkim/SVL")}
+              >
+                <p className="overlayText">다국어 지도</p>
+              </div>
+            </div>
+            <div className="project">
+              <img src={project2} alt="project2" width={300} />
+              <div
+                className="overlay"
+                onClick={() => window.open("https://github.com/dustkim/AI-sanjae")}
+              >
+                <p className="overlayText">AI산재판별</p>
+              </div>
+            </div>
+            <div className="project">
+              <img src={project3} alt="project3" width={300} />
+              <div
+                className="overlay"
+                onClick={() => window.open("https://github.com/dustkim/Auto-Aware")}
+              >
+                <p className="overlayText">드라이빙 케어</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
--- portfolio/src/Component/Skill.js
+++ portfolio/src/Component/Skill.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CSS/Skill.css";
 import skill from "../Image/skill.png";
+import python from "../Image/python.jpeg";
+import vscode from "../Image/vscode.png";
+import aws from "../Image/aws.png";
+import gcp from "../Image/GCP.png";
+import next from "../Image/next.png";
+import back from "../Image/back.png";
 
 const Skill = () => {
+  const [index, setIndex] = useState(0);
+  const images = [python, vscode, aws, gcp];
+
+  const Prev = () => {
+    if (index > 0) setIndex(index - 1);
+    else if (index <= 0) setIndex(images.length - 1);
+  };
+
+  const Next = () => {
+    if (index < images.length - 1) setIndex(index + 1);
+    else if (index >= images.length - 1) setIndex(0);
+  };
   return (
     <div className="TotalSkillBox">
       <div className="SkillContainer">
@@ -28,6 +46,23 @@ const Skill = () => {
                 <li>React</li>
               </ul>
             </div>
+          </div>
+          <div className="SkillBox">
+            <button onClick={Prev}>
+              <img src={back} alt="back" width={50} />
+            </button>
+            <div className="SkillImageRap">
+              <div className="SkillImage" style={{ transform: `translateX(-${index * 100}%)` }}>
+                {images.map((img, i) => (
+                  <div className="ImageSlide" key={i}>
+                    <img src={img} alt={`${img}`} />
+                  </div>
+                ))}
+              </div>
+            </div>
+            <button onClick={Next}>
+              <img src={next} alt="next" width={50} />
+            </button>
           </div>
         </div>
       </div>
